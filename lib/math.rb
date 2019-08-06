0feca237dd78555a4dce64ffbf1e22d711607619
Mykle jones <mykle.j91@gmail.com>
2019-08-06 21:06:09 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -7,7 +7,7 @@ return num1-num2
 end
 
 def division(num1, num2)
-return num1%num2
+return num1/num2
 end
 
 def multiplication(num1, num2)
@@ -15,7 +15,7 @@ return num1*num2
 end
 
 def modulo(num1, num2)
-
+return num1%num2
 end
 
 def square_root(num)
